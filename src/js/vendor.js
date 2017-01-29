a0f8091477a4e24a10c66eb08ea9c1085f9bd0be
Anton Shulzhenko <shulzhenkomailbox@gmail.com>
2017-01-29 17:28:42 +0200

    finished banner

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,3 +1,4 @@
 //=require 'jquery/dist/jquery.min.js'
 //=require 'bootstrap-sass/assets/javascripts/bootstrap.min.js'
 //=require 'gsap/src/minified/TweenMax.min.js'
+//=require 'libs/owl.carousel.min.js'
